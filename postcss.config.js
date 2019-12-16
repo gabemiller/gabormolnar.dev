b4d363fc65341baa6e5e8bc92f2769d7df692df3
Gabor Molnar <hello@gabormolnar.dev>
2019-12-16 19:35:41 +0100

    Refactor postcss.config.js - extract autprefixer Indent package.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
+const plugins = [
+	require('autoprefixer'),
+];
+
 if(process.env.NODE_ENV === 'production') {
-	module.exports = {
-		plugins: [
-			require('autoprefixer'),
-			require('cssnano'),
-		]
-	}
+	plugins.push(require('cssnano'));
 }
+
+module.exports = { plugins };
